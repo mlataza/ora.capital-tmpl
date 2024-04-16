ee5b80a96dfb7ee5a569cf1eb30473fee10d233c
mlataza <1750947+mlataza@users.noreply.github.com>
2024-04-17 05:27:45 +0800

    wrap commands on youtube iframe player ready

--- styles/assets/app/video.js
+++ styles/assets/app/video.js
@@ -8,41 +8,45 @@ var modalContainer = $(`
 <div class="modal vh-100 w-100 align-items-center justify-content-center d-none" style="background: rgba(0, 0, 0, 0.5);"></div>
 `).appendTo('body');
 
-$('#video-1').each(function () {
-    var video = $(this);
-    var parent = $(this).parent();
-    var container = $('.dashboard__wallet-item-head', video);
-    var shownPopup = false;
-    var cover = $('.video-cover', video);
-    var player = new YT.Player('video-1-player');
+function onYouTubeIframeAPIReady() {
 
-    function showPopup() {
-        container.css('height', '100%');
-        video.appendTo(modalContainer).css({height: '80%', width: '80%'});
-        cover.css('display', 'none');
+    $('#video-1').each(function () {
+        var video = $(this);
+        var parent = $(this).parent();
+        var container = $('.dashboard__wallet-item-head', video);
+        var shownPopup = false;
+        var cover = $('.video-cover', video);
+        var player = new YT.Player('video-1-player');
 
-        modalContainer.removeClass('d-none').addClass('d-flex').on('click', function() {
-            shownPopup = false;
-            hidePopup(); 
-        });
-    }
+        function showPopup() {
+            container.css('height', '100%');
+            video.appendTo(modalContainer).css({ height: '80%', width: '80%' });
+            cover.css('display', 'none');
 
-    function hidePopup() {
-        container.css('height', '242px');
-        video.appendTo(parent).css({height: 'auto', width: '100%'});
-        cover.css('display', 'block');
-
-        modalContainer.removeClass('d-flex').addClass('d-none').off('click');
-        player.pauseVideo();
-    }
-
-    $(video).on('click', function() {
-        shownPopup = !shownPopup;
-
-        if (shownPopup) {
-            showPopup();
-        } else {
-            hidePopup();
+            modalContainer.removeClass('d-none').addClass('d-flex').on('click', function () {
+                shownPopup = false;
+                hidePopup();
+            });
         }
+
+        function hidePopup() {
+            container.css('height', '242px');
+            video.appendTo(parent).css({ height: 'auto', width: '100%' });
+            cover.css('display', 'block');
+
+            modalContainer.removeClass('d-flex').addClass('d-none').off('click');
+            player.pauseVideo();
+        }
+
+        $(video).on('click', function () {
+            shownPopup = !shownPopup;
+
+            if (shownPopup) {
+                showPopup();
+            } else {
+                hidePopup();
+            }
+        });
     });
-});
+
+};
